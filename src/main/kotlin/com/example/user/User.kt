fa4d6a55c673c102420393a45a5664af5e9c0842
kevin collier <collier.kevin.m@gmail.com>
2024-01-09 20:22:26 +0900

    created and passed tests for email validation

--- src/main/kotlin/com/example/user/User.kt
+++ src/main/kotlin/com/example/user/User.kt
@@ -1,5 +1,7 @@
 package com.example.user
 
 class User(val id: String, val firstName: String, val lastName: String, val email: String) {
-
+    init {
+        require(email.contains("@") && email.contains(".")) {"Invalid email format"}
+    }
 }
--- src/test/kotlin/com/example/user/UserTest.kt
+++ src/test/kotlin/com/example/user/UserTest.kt
@@ -2,6 +2,8 @@ package com.example.user
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertThrows
+
 internal class UserTest {
     @Test
     fun `create user with required fields`() {
@@ -10,5 +12,17 @@ internal class UserTest {
         assertEquals("John", user.firstName)
         assertEquals("Doe", user.lastName)
         assertEquals("john.doe@example.com", user.email)
+    }
+    @Test
+    fun `user email format is valid`() {
+        val user = User("1", "John", "Doe", "john.doe@example.com")
+        assertTrue(user.email.contains("@") && user.email.contains("."))
+    }
+
+    @Test
+    fun `user email format is invalid`() {
+        assertThrows<IllegalArgumentException> {
+            User("1", "John", "Doe", "fakeemail")
+        }
     }
 }
