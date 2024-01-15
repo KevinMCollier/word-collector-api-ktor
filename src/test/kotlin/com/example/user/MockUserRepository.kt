192121f652010c42ac82c25835c7546d76df75cc
kevin collier <collier.kevin.m@gmail.com>
2024-01-15 18:32:21 +0900

    added and passed UserRepository test for returning false when add user fails

--- src/test/kotlin/com/example/user/MockUserRepository.kt
+++ src/test/kotlin/com/example/user/MockUserRepository.kt
@@ -2,9 +2,14 @@ package com.example.user
 
 class MockUserRepository: UserRepository {
     var addInvoked = false
+    var addUserSuccess = true
     override fun add(user: User): Boolean {
-        addInvoked = true
-        return true
+        if (addUserSuccess) {
+            addInvoked = true
+            return true
+        } else {
+            return false
+        }
     }
-
 }
+
--- src/test/kotlin/com/example/user/UserRepositoryTest.kt
+++ src/test/kotlin/com/example/user/UserRepositoryTest.kt
@@ -2,6 +2,7 @@ package com.example.user
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertTrue
+import kotlin.test.assertFalse
 
 internal class UserRepositoryTest {
     private lateinit var repository: MockUserRepository
@@ -9,10 +10,22 @@ internal class UserRepositoryTest {
     @Test
     fun `add method should return true when a new user is added successfully`() {
         repository = MockUserRepository()
+        repository.addUserSuccess = true
         testUser = User("1", "John", "Doe", "john.doe@example.com")
 
         val result = repository.add(testUser)
         assertTrue(result)
         assertTrue(repository.addInvoked)
+    }
+
+    @Test
+    fun `add method should return false when adding a new user fails`() {
+        repository = MockUserRepository()
+        repository.addUserSuccess = false
+        testUser = User("1", "John", "Doe", "john.doe@example.com")
+
+        val result = repository.add(testUser)
+        assertFalse(result)
+        assertFalse(repository.addInvoked)
     }
 }
